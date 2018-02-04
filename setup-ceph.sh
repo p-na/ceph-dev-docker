8cb59efc1d56edbe40d7627da546af2b35824757
Patrick Nawracay <pnawracay@suse.com>
2018-02-04 23:03:19 +0100

    Install all dependecies on image creation

--- setup-ceph.sh
+++ setup-ceph.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-set -e
-
 cpus=$(lscpu | sed -n 's/^CPU(s):\s*\(\d*\)/\1/p')
 
 cd /ceph
 git clone https://github.com/openattic/ceph .
 ./install-deps.sh
-./do_cmake.sh
-cd /ceph/build
-make -j${cpus}
+
+if [ ! -d /ceph/build ]; then
+    ./do_cmake.sh
+    cd /ceph/build
+    make -j${cpus}
+else
+    echo "skipping compilation... found /ceph/build"
+fi
 
 pip install -r /ceph/src/pybind/mgr/dashboard_v2/requirements.txt
