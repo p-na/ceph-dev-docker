030997c019c2b5254fe5dd63c873653d39fc35ed
Patrick Nawracay <patrick@nawracay.de>
2018-02-13 11:16:08 +0100

    Make `setup-ceph` usable for recompiling Ceph

--- setup-ceph.sh
+++ setup-ceph.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+set -ex
+
 cd /ceph
-git clone https://github.com/openattic/ceph .
+git clone https://github.com/openattic/ceph . | true
 ./install-deps.sh
 
+cmake_called=False
 if [ ! -d /ceph/build ]; then
     ./do_cmake.sh -DWITH_PYTHON3=ON -DWITH_TESTS=OFF
+    cmake_called=True
 fi
 cd /ceph/build
+if [ "${cmake_called}" -eq "False" ] ; then
+    cmake -DWITH_CCACHE=ON -DWITH_TESTS=OFF -DWITH_PYTHON3=ON ..
+fi
 make -j$(nproc)
 
 pip2 install -r /ceph/src/pybind/mgr/dashboard_v2/requirements.txt
